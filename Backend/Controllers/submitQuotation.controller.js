2c359e9c46976650bae0829e6f4e81daa2f82b56
om-sairaj-rout <omsairajraut@gmail.com>
2026-03-28 00:49:50 +0530

    fix email problem

--- Backend/Controllers/submitQuotation.controller.js
+++ Backend/Controllers/submitQuotation.controller.js
@@ -6,7 +6,9 @@ const submitQuotationController = async (req, res) => {
     const file = req.file;
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+       host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // SSL
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
